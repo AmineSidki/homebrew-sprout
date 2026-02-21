905a870704f27688171ce777d9838f3bbfa00a12
jreleaserbot <jreleaser@kordamp.org>
2026-02-21 12:55:38 +0000

    sprout v1.6.1

--- Formula/sprout.rb
+++ Formula/sprout.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.13.1 at 2026-02-21T12:19:39.898067499Z
+# Generated with JReleaser 1.13.1 at 2026-02-21T12:55:23.27212979Z
 
 class Sprout < Formula
   desc "Sprout CLI scaffolding tool"
   homepage "https://github.com/AmineSidki/sprout-releases"
   url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.6.1/sprout-osx-x86_64.zip"
   version "1.6.1"
-  sha256 "73f9858078593a90a4458ac3864cd3e005806e0d0c6bfa51065bcc9ec5b35e0d"
+  sha256 "3c3eb9ac62d3a543caf56a720d3fbb11968facf0cc6ed55a906ebfcfd528c6fe"
   license "MIT"
 
 
