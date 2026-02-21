fec747effd3455ddfaa452cb99de2f3d67ae5ae1
jreleaserbot <jreleaser@kordamp.org>
2026-02-21 13:19:56 +0000

    sprout v1.6.3

--- Formula/sprout.rb
+++ Formula/sprout.rb
@@ -1,12 +1,19 @@
-# Generated with JReleaser 1.13.1 at 2026-02-21T12:55:23.27212979Z
+# Generated with JReleaser 1.13.1 at 2026-02-21T13:19:42.950142373Z
 
 class Sprout < Formula
   desc "Sprout CLI scaffolding tool"
   homepage "https://github.com/AmineSidki/sprout-releases"
-  url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.6.1/sprout-osx-x86_64.zip"
-  version "1.6.1"
-  sha256 "3c3eb9ac62d3a543caf56a720d3fbb11968facf0cc6ed55a906ebfcfd528c6fe"
+  version "1.6.3"
   license "MIT"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.6.3/sprout-osx-x86_64.zip"
+    sha256 "11738a6ebfcfd1ddfb0594de123d98fefd898d6220afb641b9f84c0e4631886b"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.6.3/sprout-osx-x86_64.zip"
+    sha256 "11738a6ebfcfd1ddfb0594de123d98fefd898d6220afb641b9f84c0e4631886b"
+  end
 
 
   def install
@@ -16,6 +23,6 @@ class Sprout < Formula
 
   test do
     output = shell_output("#{bin}/sprout --version")
-    assert_match "1.6.1", output
+    assert_match "1.6.3", output
   end
 end
