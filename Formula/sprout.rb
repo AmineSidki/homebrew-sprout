4d56525b3edfedb5dfe7ccaddacb1c935bb4a574
jreleaserbot <jreleaser@kordamp.org>
2026-06-30 15:54:56 +0000

    sprout v1.9.3

--- Formula/sprout.rb
+++ Formula/sprout.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.13.1 at 2026-06-30T14:14:36.185761516Z
+# Generated with JReleaser 1.13.1 at 2026-06-30T15:54:46.870003373Z
 
 class Sprout < Formula
   desc "Sprout CLI scaffolding tool"
   homepage "https://github.com/AmineSidki/sprout-releases"
-  url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.9.2/sprout-1.9.2.zip"
-  version "1.9.2"
-  sha256 "f32c286ad86c2648d3562a4b82a3dbe6c4eaf2e10200ee47905878cf649b494d"
+  url "https://github.com/AmineSidki/sprout-releases/releases/download/v1.9.3/sprout-1.9.3.zip"
+  version "1.9.3"
+  sha256 "2f5af8cbf0753fdb7deb282e9212594cfbd818c25959b053aa5bc41a2549a3f3"
   license "MIT"
 
   depends_on "openjdk@17"
@@ -17,6 +17,6 @@ class Sprout < Formula
 
   test do
     output = shell_output("#{bin}/sprout --version")
-    assert_match "1.9.2", output
+    assert_match "1.9.3", output
   end
 end
